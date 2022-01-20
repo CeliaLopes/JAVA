064a92535bd0888cbb8d18b86431ac0614424cad
CeliaLopes <oliveira.lopes.celia@gmail.com>
2022-01-20 16:54:20 -0300

    criacao de calculadora

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -7,21 +7,12 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livros livros = new Livros();
 		
 		System.out.println(gato);
-		System.out.println(livros);
-		/*int a = 2;
-		int b = 4;
-		System.out.println("Hello Word ! " + (a+b));*/
 		
 	}
 	
-
 }
 
-class Livros{
-	private String nome;
-	private String npag;
+
 	
-}
